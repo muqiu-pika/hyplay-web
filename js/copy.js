781fb4114695862eeaa301cd0fd91b100aa37d62
慕秋_pika <muqiu_pika@outlook.com>
2025-04-04 21:43:29 +0800

    优化界面，减少字体库大小

--- js/copy.js
+++ js/copy.js
@@ -1,21 +1,34 @@
-function showCustomAlert(message) {
-    // 创建提示框元素
+function showCustomAlert(message, isSuccess) {
     const alertBox = document.createElement('div');
-    alertBox.textContent = message;
+    const icon = document.createElement('i');
+    icon.className = isSuccess ? 'fa-regular fa-circle-check' : 'fa-regular fa-circle-xmark';
+    icon.style.color = isSuccess ? 'green' : 'red';
+    alertBox.appendChild(icon);
+
+    const text = document.createTextNode(' ' + message);
+    alertBox.appendChild(text);
+
     alertBox.style.position = 'fixed';
-    alertBox.style.top = '50px'; // 顶栏正下方
+    alertBox.style.top = '50px';
     alertBox.style.left = '50%';
     alertBox.style.transform = 'translateX(-50%)';
-    alertBox.style.backgroundColor = '#333';
-    alertBox.style.color = '#fff';
     alertBox.style.padding = '10px 20px';
     alertBox.style.borderRadius = '5px';
     alertBox.style.zIndex = '1000';
     alertBox.style.boxShadow = '0 2px 5px rgba(0, 0, 0, 0.3)';
-    
+
+    if (document.body.classList.contains('dark-theme')) {
+        alertBox.style.backgroundColor = '#000';
+        alertBox.style.color = '#fff';
+        alertBox.style.border = '1px solid #fff';
+    } else {
+        alertBox.style.backgroundColor = '#fff';
+        alertBox.style.color = '#000';
+        alertBox.style.border = '1px solid #5e72e4';
+    }
+
     document.body.appendChild(alertBox);
 
-    // 3秒后自动移除提示框
     setTimeout(() => {
         document.body.removeChild(alertBox);
     }, 3000);
@@ -38,9 +51,9 @@ document.querySelectorAll('.copy-button').forEach(button => {
         }
 
         navigator.clipboard.writeText(fullAddress).then(() => {
-            showCustomAlert('地址已复制到剪贴板');
+            showCustomAlert('复制成功，快去加入吧！', true);
         }).catch(err => {
-            console.error('复制失败: ', err);
+            showCustomAlert('复制失败，请检查权限！', false);
         });
     });
 });
